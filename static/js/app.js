cf03884045418325311aec6928f3b9d28f606af2
KevinKronk <kjkronk@ncsu.edu>
2022-11-13 15:41:18 -0500

    Build the table

--- static/js/app.js
+++ static/js/app.js
@@ -8,3 +8,21 @@ var tbody = d3.select("tbody");
 function printHello() {
     console.log("Hello there!");
 }
+
+// Build a table
+function buildTable(data) {
+    // Clear out existing data
+    tbody.html("");
+    // Loop through each object in the data array
+    data.forEach((dataRow) => {
+        // Append a row to the table
+        let row = tbody.append("tr");
+        // grab the values in the dataRow and loop through each
+        Object.values(dataRow).forEach((val) => {
+            // Append a table data point 
+            let cell = row.append("td");
+            // Add the value to the text of the "td"
+            cell.text(val);
+        });
+    });
+}
